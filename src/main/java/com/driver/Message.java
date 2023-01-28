de39e7d7231104f404944dd03a92a0f907daafb5
Rehan Sakiwala <rehansakiwala@gmail.com>
2023-01-28 17:49:50 +0530

    Fixed5

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,13 +7,21 @@ public class Message {
     private String content;
     private Date timestamp;
 
-    public Message(int id, String content,Date date) {
+    public Message(int id) {
+        this.id = id;
+        this.timestamp = new Date();
+    }
+
+    public Message(int id, String content) {
         this.id = id;
         this.content = content;
         this.timestamp = new Date();
     }
-
-    //Getters and setters
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
 
     public int getId() {
         return id;
