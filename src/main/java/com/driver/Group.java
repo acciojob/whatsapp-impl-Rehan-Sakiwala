d516114337f504456a7b7c91dd29a91ea87fefc7
Rehan Sakiwala <rehansakiwala@gmail.com>
2023-01-28 16:37:29 +0530

    Transfered from whatsapp-rehan

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -4,4 +4,32 @@ public class Group {
     private String name;
     private int numberOfParticipants;
 
+    //Constructor
+
+
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public Group(){
+
+    }
+    //Getters And Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
 }
